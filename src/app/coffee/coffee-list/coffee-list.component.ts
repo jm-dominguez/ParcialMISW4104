794978cbe0f14f2c47418a055dafc49c7b33b4ad
Juan Manuel <jm.dominguez@uniandes.edu.co>
2025-04-25 19:10:20 -0500

    Create coffee module and folder structure

---
+++ src/app/coffee/coffee-list/coffee-list.component.ts
@@ -0,0 +1,11 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-coffee-list',
+  imports: [],
+  templateUrl: './coffee-list.component.html',
+  styleUrl: './coffee-list.component.css'
+})
+export class CoffeeListComponent {
+
+}
---
+++ src/app/coffee/services/coffee.service.ts
@@ -0,0 +1,23 @@
+import { HttpClient } from '@angular/common/http';
+import { inject, Injectable } from '@angular/core';
+
+@Injectable({
+  providedIn: 'root'
+})
+export class CoffeeService {
+
+  private baseUrl = "https://gist.githubusercontent.com/josejbocanegra/e9d24db370ce95b75555f7d1f8691805/raw/8a26ac2bca4183dc88545e14c45851d698911358/202212_MISW4104_Grupo3.json";
+  private http = inject(HttpClient);
+
+  constructor() { }
+
+  /**
+   * Method to get a list of coffees from the url
+   * @returns list of Coffees
+   */
+  getCoffees() {
+    return this.http.get(this.baseUrl).subscribe((coffees) => {
+      return coffees;
+    })
+  }
+}
