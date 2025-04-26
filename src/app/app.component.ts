b6cd966033524bbfc9b7d6cc9f06a0b250ad7e00
Juan Manuel <jm.dominguez@uniandes.edu.co>
2025-04-25 19:10:51 -0500

    Update app component to call the coffee-list component

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,9 +1,10 @@
 import { Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
+import { CoffeeListComponent } from "./coffee/coffee-list/coffee-list.component";
 
 @Component({
   selector: 'app-root',
-  imports: [RouterOutlet],
+  imports: [RouterOutlet, CoffeeListComponent],
   templateUrl: './app.component.html',
   styleUrl: './app.component.css'
 })
